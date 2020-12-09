5aa857c53914406a1d8d9d7b8780cbcd63943ba4
Adam Klodowski <adam.klodowski@lut.fi>
2020-12-10 00:01:55 +0200

    Header now includes link to Version.txt

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div class="w3-display-container" style="position:relative;">
 <div class="logout" onclick="location.href='?action=logout';"></div>	
-<div class="version"><?php echo $version; ?></div>	
+<div class="version"><a href="Version.txt" target="_blank"><?php echo $version; ?></a></div>	
 <img src="mosim.png" style="width:50%; margin-left:25%; margin-right:25%;" alt="Logo" />
 <div class="w3-display-bottomleft w3-container w3-text-black"><h2></h2></div>
 </div>
